244eb055a27265a8ee881830a4015df93d6975a5
Sagar Patel <17.sagar.patel@gmail.com>
2014-03-02 02:00:07 +0900

    Pseudo code for graphin values

---
+++ UnityFolder/Assets/Scripts/SliceView.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliceView : MonoBehaviour 
+{
+
+	public string independantVariable
+	public string dependantVariable;
+	public int year;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void SetValues()
+	{
+		// get list of independantVariables based on string, ex: if independantVariable = "Manufacturer", the returned list should be "Acura, Aston Martin,..."
+
+		// for each element in above list, get associated value for given year
+
+		// use iTween to lerp gameobject scale to new value
+	}
+
+
+}
